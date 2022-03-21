1228706070fc94b2ef7e6831d2f180f6d1a259ef
MarcosRidley <marcosridley@hotmail.com>
2022-03-21 19:55:54 -0300

    fixed css issues with zoom

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let firstBoard = true;
 
 function addListenersToColors() {
-  const colors = document.querySelectorAll('.color');
-  for (let color of colors) {
-    color.addEventListener('click', changeActiveColor);
-  }
+	const colors = document.querySelectorAll('.color');
+	for (let color of colors) {
+		color.addEventListener('click', changeActiveColor);
+	}
 }
 function addListenersToPixels() {
-  const pixels = document.querySelectorAll('.pixel');
-  for (let pixel of pixels) {
-    pixel.addEventListener('click', paintPixel);
-  }
+	const pixels = document.querySelectorAll('.pixel');
+	for (let pixel of pixels) {
+		pixel.addEventListener('click', paintPixel);
+	}
 }
 function addListenersToButtons() {
-  document.querySelector('#clear-board').addEventListener('click', clearPixels);
-  document
-    .getElementById('generate-board')
-    .addEventListener('click', generateBoard);
+	document.querySelector('#clear-board').addEventListener('click', clearPixels);
+	document
+		.getElementById('generate-board')
+		.addEventListener('click', generateBoard);
 }
 addListenersToColors();
 makeColorsRandom();
@@ -24,71 +24,71 @@ addListenersToButtons();
 addListenersToPixels();
 
 function changeActiveColor(event) {
-  document.querySelector('.selected').classList.remove('selected');
-  event.target.classList.add('selected');
+	document.querySelector('.selected').classList.remove('selected');
+	event.target.classList.add('selected');
 }
 
 function paintPixel(event) {
-  event.target.style.backgroundColor =
-    document.querySelector('.selected').style.backgroundColor;
+	event.target.style.backgroundColor =
+		document.querySelector('.selected').style.backgroundColor;
 }
 
 function clearPixels() {
-  const pixels = document.querySelectorAll('.pixel');
-  for (let pixel of pixels) {
-    pixel.style.backgroundColor = 'white';
-  }
+	const pixels = document.querySelectorAll('.pixel');
+	for (let pixel of pixels) {
+		pixel.style.backgroundColor = 'white';
+	}
 }
 function generatePixels() {
-  let boardSide = document.getElementById('board-size').value;
-  if (!boardSide) {
-    alert('Board inválido!');
-    return;
-  }
-  if (boardSide < 5) boardSide = 5;
-  if (boardSide > 50) boardSide = 50;
-  const container = document.getElementById('pixel-board');
-  for (let i = 0; i < boardSide * boardSide; i++) {
-    const newPixel = document.createElement('div');
-    newPixel.className = 'pixel generated';
-    newPixel.style.backgroundColor = 'white';
-    container.appendChild(newPixel);
-  }
-  document
-    .querySelector(':root')
-    .style.setProperty('--numero-width-board', 42.5 * boardSide + 'px');
-  addListenersToPixels();
+	let boardSide = document.getElementById('board-size').value;
+	if (!boardSide) {
+		alert('Board inválido!');
+		return;
+	}
+	if (boardSide < 5) boardSide = 5;
+	if (boardSide > 50) boardSide = 50;
+	const container = document.getElementById('pixel-board');
+	for (let i = 0; i < boardSide * boardSide; i++) {
+		const newPixel = document.createElement('div');
+		newPixel.className = 'pixel generated';
+		newPixel.style.backgroundColor = 'white';
+		container.appendChild(newPixel);
+	}
+	document
+		.querySelector(':root')
+		.style.setProperty('--numero-width-board', 42 * boardSide + 'px');
+	addListenersToPixels();
 }
 
 function generateBoard() {
-  if (firstBoard) {
-    const defaultPixels = document.querySelectorAll('.default-pixel');
-    for (let pixel of defaultPixels) {
-      pixel.remove();
-      firstBoard = false;
-    }
-  }
-  if (!firstBoard) {
-    const generatedPixels = document.querySelectorAll('.generated');
-    for (let pixel of generatedPixels) {
-      pixel.remove();
-    }
-  }
-  generatePixels();
+	if (firstBoard) {
+		const defaultPixels = document.querySelectorAll('.default-pixel');
+		for (let pixel of defaultPixels) {
+			pixel.remove();
+			firstBoard = false;
+		}
+	}
+	if (!firstBoard) {
+		const generatedPixels = document.querySelectorAll('.generated');
+		for (let pixel of generatedPixels) {
+			pixel.remove();
+		}
+	}
+	generatePixels();
 }
 
 function makeColorsRandom() {
-  const randomColors = document.querySelectorAll('.random');
-  for (let i of randomColors) {
-    i.style.backgroundColor = getRandomRgb();
-  }
+	const randomColors = document.querySelectorAll('.random');
+	for (let i of randomColors) {
+		i.style.backgroundColor = getRandomRgb();
+	}
 }
 
 function getRandomRgb() {
-  var num = Math.round(0xffffff * Math.random());
-  var r = num >> 16;
-  var g = (num >> 8) & 255;
-  var b = num & 255;
-  return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+	var num = Math.round(0xffffff * Math.random());
+	var r = num >> 16;
+	var g = (num >> 8) & 255;
+	var b = num & 255;
+	return 'rgb(' + r + ', ' + g + ', ' + b + ')';
 }
 //Credits to CORY, 2014, at https://stackoverflow.com/questions/23095637/how-do-you-get-random-rgb-in-javascript for the random rgb generation function
